e557ba39e838c4574541c2cd9f07b982668d16a7
Jinhyung Park <jenix@app4.me>
2011-12-22 17:13:37 -0500

    Fix RTT

--- src/com/columbia/ng911/NG911Activity.java
+++ src/com/columbia/ng911/NG911Activity.java
@@ -766,6 +766,7 @@ public class NG911Activity extends Activity {
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
+		sipController.hangup();
 		if(killProcess)
 			//finish();
 			android.os.Process.killProcess(android.os.Process.myPid());
